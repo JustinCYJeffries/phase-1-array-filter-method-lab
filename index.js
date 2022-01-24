10b4032cea8e907fb3b9df59cf143a127f3ef31c
Justin Jeffries <justincyjeffries@gmail.com>
2022-01-24 00:14:21 -0700

    commit tree 13dde23f3a53bc8d077b53361e43aa8907c48dea

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string){
+let match = drivers.filter(obj => obj === string || obj === string.toLowerCase())
+return match
+}
+
+function fuzzyMatch(drivers, string){
+    let letter = drivers.filter(driver => driver.slice(0, string.length) === string)
+    return letter
+}
+
+function matchName(drivers, string){
+    let match = drivers.filter(obj => obj.name === string)
+    return match
+}
